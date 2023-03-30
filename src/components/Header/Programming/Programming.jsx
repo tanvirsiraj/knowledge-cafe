d247eecef08faea19dd98716e0657e57c50fc007
tanvirhasn <tanvirhasan.edu.bd@gmail.com>
2023-03-31 03:33:54 +0600

    programmer component added

--- src/components/Header/Programming/Programming.jsx
+++ src/components/Header/Programming/Programming.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Programmer from '../../Programmer/Programmer';
 import './Programming.css'
 
 const Programming = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, [])
+
     return (
         <div className='programming-container container row row-cols-sm-1 row-cols-md-2 mx-auto'>
             <div className="programmers col-8">
-                <h1>Programmer details</h1>
+                <Programmer></Programmer>
             </div>
             <div className="dashboard col-4">
                 <h3>Spent time on read: </h3>
---
+++ src/components/Programmer/Programmer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Programmer.css';
+
+const Programmer = ({ }) => {
+    return (
+        <div className='programmer-block'>
+
+        </div>
+    );
+};
+
+export default Programmer;
