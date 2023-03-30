620d0d70dd9812a07ae910b7f5fb10610c5e9da1
tanvirhasn <tanvirhasan.edu.bd@gmail.com>
2023-03-31 05:47:47 +0600

    details added to Programmer component

--- src/components/Header/Programming/Programming.jsx
+++ src/components/Header/Programming/Programming.jsx
@@ -12,11 +12,13 @@ const Programming = () => {
     }, [])
 
     return (
-        <div className='programming-container container row row-cols-sm-1 row-cols-md-2 mx-auto'>
-            <div className="programmers col-8">
-                <Programmer></Programmer>
+        <div className='programming-container container row row-cols-1 row-cols-md-2 mx-auto'>
+            <div className="programmers col-sm-12 col-md-8">
+                {
+                    data.map(data => <Programmer key={data.id} data={data}></Programmer>)
+                }
             </div>
-            <div className="dashboard col-4">
+            <div className="dashboard col-sm-12 col-md-4">
                 <h3>Spent time on read: </h3>
             </div>
         </div>
--- src/components/Programmer/Programmer.jsx
+++ src/components/Programmer/Programmer.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import './Programmer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 
-const Programmer = ({ }) => {
+
+const Programmer = (props) => {
+    const { authorName, blogTitle, coverImage, authorImage, readTime, publishDate } = props.data;
     return (
-        <div className='programmer-block'>
-
+        <div className='programmer-block mb-5'>
+            <img className='cover-img' src={coverImage} alt="" />
+            <div className="programmer-details">
+                <div className="personal-info">
+                    <img className='author-img' src={authorImage} alt="" />
+                    <div>
+                        <h3>{authorName}</h3>
+                        <span>{publishDate}</span>
+                    </div>
+                </div>
+                <div>
+                    <span className='reading-time'>{readTime} min read</span>
+                    <FontAwesomeIcon className='book-mark' icon={faBookmark} />
+                </div>
+            </div>
+            <h1 className="blog-title">
+                {blogTitle}
+            </h1>
+            <button className='mark-read'>Mark as read</button>
         </div>
     );
 };
